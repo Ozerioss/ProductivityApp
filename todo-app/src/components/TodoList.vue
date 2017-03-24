24380ab79586712676d81fb2e7d97c54354ee602
Karim Kidiss <kkidiss@yahoo.fr>
2017-03-24 14:18:47 +0100

    Added the new vue, and fixed errors with the eslint

---
+++ todo-app/src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<ul>
+			<li> test A </li>
+			<li> test B </li>
+			<li> test C </li>
+		</ul>
+	</div>
+</template>
+
+<script type = "text/javascript">
+
+export default
+{
+}
+
+</script>
+
+<style> 
+</style>
